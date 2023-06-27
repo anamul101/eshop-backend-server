1cb4b0111e0709d3e48366597498874777927f42
anamul101 <rjanamul01871@gmail.com>
2023-06-27 21:12:35 +0100

    corse

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path =require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({credentials:true}));
 app.use("/", express.static("./uploads"));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
