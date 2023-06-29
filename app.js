0d5363d9296f585555b04fb46b856910a15fff66
anamul101 <rjanamul01871@gmail.com>
2023-06-29 11:35:39 +0100

    jwt ubdate

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ const path =require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  allowedHeaders:"*",
-  allowMethods:"*",
-  origin:"*"
-}));
+app.use(cors());
 app.use("/", express.static("./uploads"));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
