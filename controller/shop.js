2ba6a6c2751e13e670653b221b0152ba54b2827c
anamul101 <rjanamul01871@gmail.com>
2023-07-03 20:03:16 +0100

    ksdj

--- controller/shop.js
+++ controller/shop.js
@@ -175,8 +175,7 @@ router.get(
       res.cookie("seller_token", null, {
         expires: new Date(Date.now()),
         httpOnly: true,
-        sameSite: "none",
-        secure: false,
+        secure: true
       });
       res.status(201).json({
         success: true,
