17b4b087a9b2e7626c8d05fddd610422b89c5bf8
anamul101 <rjanamul01871@gmail.com>
2023-07-06 17:52:00 +0100

    nckjd

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(cors({
-  origin: 'https://shop-frontend-three.vercel.app/',
+  origin: 'https://shop-frontend-three.vercel.app',
   credentials: true
 }));
 app.use(express.json());
